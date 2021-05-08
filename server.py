f769745b4c500a065da5c4dc2f1db8fe3ab2a989
Leandro Pontes Berleze <leandropberleze@gmail.com>
2021-05-08 15:41:41 -0300

    Feat: add base HTTP server

---
+++ server.py
@@ -0,0 +1,33 @@
+from http.server import HTTPServer, SimpleHTTPRequestHandler
+from socketserver import ThreadingMixIn
+import threading
+
+HOST = '127.0.0.1'
+PORT = 6789
+
+class ThreadingSimpleServer(ThreadingMixIn, HTTPServer):
+    pass
+
+class HttpRequestHandler(SimpleHTTPRequestHandler): 
+
+    def do_GET(self):
+        self.send_response(200)
+        self.end_headers()
+        message =  threading.currentThread().getName()
+        self.wfile.write(message.encode('utf8'))
+        return
+
+
+
+def Main():
+    httpServer = ThreadingSimpleServer((HOST, PORT), HttpRequestHandler)
+
+    try:
+        httpServer.serve_forever()
+    except KeyboardInterrupt:
+        pass
+    httpServer.server_close()
+    print("Parando servidor...")
+
+if __name__ == '__main__':
+    Main()
