14a3c56ec16c5f8a100756f220eb07c4ae63f389
Leandro Pontes Berleze <leandropberleze@gmail.com>
2021-05-09 18:06:10 -0300

    Feat: add handle request

--- server.py
+++ server.py
@@ -1,6 +1,7 @@
 from http.server import HTTPServer, BaseHTTPRequestHandler
 from socketserver import ThreadingMixIn
 import threading
+import codecs
 
 HOST = '127.0.0.1'
 PORT = 6789
@@ -10,12 +11,28 @@ class ThreadingSimpleServer(ThreadingMixIn, HTTPServer):
 
 class HttpRequestHandler(BaseHTTPRequestHandler): 
     def do_GET(self):
-        self.send_response(200)
+        print(self.headers)
+        
+        self.handle_request()
+
+    def handle_request(self):
+        message = ''
+        if('index.html' in self.path):
+            self.send_response(200)
+            message = codecs.open("index.html", 'r')
+        else:
+            self.send_response(404)
+            message = codecs.open("404.html", 'r')
+        
         self.send_header('Connection', 'close')
         self.end_headers()
-        message =  threading.currentThread().getName()
-        self.wfile.write(message.encode('utf8'))
-        return
+
+        self.wfile.write(message.read().encode('utf8'))
+
+    def send_response(self, code, message=None):
+        self.send_response_only(code, message)
+        self.send_header('Server', self.version_string())
+        self.send_header('Date', self.date_time_string())
 
 def Main():
     httpServer = ThreadingSimpleServer((HOST, PORT), HttpRequestHandler)
